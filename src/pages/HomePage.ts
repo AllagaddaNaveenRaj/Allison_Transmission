fbc4abf45caf8f17f17011ac7b894c8b80a7c96f
A Naveen <a.naveen@tavant.com>
2024-12-24 12:13:16 +0530

    Initial commit

---
+++ src/pages/HomePage.ts
@@ -0,0 +1,43 @@
+import { Page, expect } from "@playwright/test";
+//import logger from "../utils/LoggerUtil";
+//import ContactPage from "./ContactPage";
+
+export default class HomePage {
+  private readonly fourwardTitleLocator = "4WARD";
+  //private readonly contactsLinkLocator = "Contacts";
+
+  constructor(private page: Page) {}
+  async expectfourWARDTitleToBeVisible() {
+    await expect(this.page.getByTitle(this.fourwardTitleLocator)).toBeVisible({timeout: 15000})
+   }
+
+ /* async expectServiceTitleToBeVisible() {
+    await expect(this.page.getByTitle(this.serviceTitleLocator)).toBeVisible({
+      timeout: 15000,
+    }).catch((error) => {
+      logger.error(`Error clicking login button: ${error}`);
+      throw error; // rethrow the error if needed
+    }).then(()=>logger.info("Service Title is visible"));
+  }
+
+
+  async navigateToContactTab(){
+
+    await expect(this.page.getByRole('link', { name: this.contactsLinkLocator })).toBeVisible();
+    logger.info("Contacts Tab is visible")
+    await this.page.getByRole('link', { name: this.contactsLinkLocator }).click();
+    logger.info("Contacts Tab is clicked")
+    return new ContactPage(this.page);
+    
+  }
+
+  async navigateToCaseTab(){
+
+    await expect(this.page.getByRole('link', { name: this.contactsLinkLocator })).toBeVisible();
+    logger.info("Contacts Tab is visible")
+    await this.page.getByRole('link', { name: this.contactsLinkLocator }).click();
+    logger.info("Contacts Tab is clicked")
+    return new ContactPage(this.page);
+    
+  }*/
+}
